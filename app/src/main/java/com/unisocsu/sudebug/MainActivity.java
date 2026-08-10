c0fe0227e6580af6a513ec42988e79b869949e80
unisocsu <rmk0557603971@outlook.com>
2026-08-10 12:42:17 +0300

    Initial commit of clean Android project

---
+++ app/src/main/java/com/unisocsu/sudebug/MainActivity.java
@@ -0,0 +1,55 @@
+package com.unisocsu.sudebug;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnActivate = findViewById(R.id.btn_activate);
+        TextView tvStatus = findViewById(R.id.tv_status);
+
+        btnActivate.setOnClickListener(v -> {
+            try {
+                Process su = Runtime.getRuntime().exec("su");
+                DataOutputStream os = new DataOutputStream(su.getOutputStream());
+
+                os.writeBytes("setprop service.adb.tcp.port 5555\n");
+                os.writeBytes("stop adbd\n");
+                os.writeBytes("start adbd\n");
+                os.writeBytes("exit\n");
+                os.flush();
+
+                String ip = getIpAddress();
+                tvStatus.setText("ADB Activated!\nConnect via:\nadb connect " + ip + ":5555");
+            } catch (Exception e) {
+                tvStatus.setText("Error: " + e.getMessage());
+            }
+        });
+    }
+
+    private String getIpAddress() {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        return addr.getHostAddress();
+                    }
+                }
+            }
+        } catch (Exception ignored) {}
+        return "Unknown IP";
+    }
+}
